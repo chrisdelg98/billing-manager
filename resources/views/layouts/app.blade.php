78d02931e80e36bdb2f79c653256c624e9599018
Christian Arevalo <chrisdelg98@gmail.com>
2026-05-07 11:38:20 -0600

    Refactor code structure for improved readability and maintainability

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,6 +85,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}">
+        <link rel="shortcut icon" href="{{ asset('images/logo.png') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
